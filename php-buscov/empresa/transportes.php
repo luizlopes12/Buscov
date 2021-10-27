ee334e2bea655b07690c7fa093819ae7867a42a3
Luiz Lopes <luizlopes12@outlook.com>
2021-10-27 20:11:05 -0300

    assets

---
+++ php-buscov/empresa/transportes.php
@@ -0,0 +1,28 @@
+<?php
+include ("../conexao.php");
+
+	/* CADASTRO DE ONIBUS */
+    $cod_empresa = $_POST['cod_empresa'];
+  	$placaOnibus = $_POST['placa-onibus'];
+	$assentosOnibus = $_POST['assentos-onibus'];
+	$origemOnibus = $_POST['origem-onibus'];
+	$destinoOnibus = $_POST['destino-onibus'];
+
+    if(isset($_POST['register']))
+    {   
+        $register = mysqli_num_rows(mysqli_query($con, "SELECT * FROM onibus WHERE placa = '$placaOnibus'"));
+        if($register == 0)
+        {
+            $insert = mysqli_query($con, "INSERT INTO onibus (cod_empresa, placa, assentos , origem, destino) VALUES ('$cod_empresa', '$placaOnibus', '$assentosOnibus', '$origemOnibus', '$destinoOnibus')");
+
+            if($insert)
+                echo "success";
+            else
+                echo "error";
+        }
+        else if($register != 0)
+            echo "exist";
+    }
+
+    mysqli_close($con);
+?>
---
+++ php-buscov/usuario/buscar-onibus.php
@@ -0,0 +1,11 @@
+<?php
+include ("../conexao.php");
+$og = $_POST['origem'];
+$des = $_POST['destino'];
+$data=array();
+$q=mysqli_query($con,"select * from onibus where origem='$og' and destino='$des'");
+while ($row=mysqli_fetch_object($q)){
+ $data[]=$row;
+}
+echo json_encode($data);
+?>
